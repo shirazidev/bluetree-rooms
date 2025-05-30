18e21140e0b514e74c50e565325a0ef09ffbaa55
AmirHossein Shirazi <ashzih2@gmail.com>
2025-05-30 17:26:14 +0330

    feat: add ImageModule to app.module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmDbConfig } from './configs/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+import { ImageModule } from './modules/image/image.module';
 
 @Module({
-  imports: [    ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: join(process.cwd(), '.env'),
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), '.env'),
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: TypeOrmDbConfig,
     }),
-    AuthModule, UserModule, RoomsModule],
+    AuthModule,
+    UserModule,
+    RoomsModule,
+    ImageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
