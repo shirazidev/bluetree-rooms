03c83337aa0e8aff84bc4fab22cef3c5c754ee94
ashzih2 <ashzih2@gmail.com>
2025-09-13 21:54:03 +0000

    00

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { forwardRef, Module, Global } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './guards/auth.guard';
 
+@Global()
 @Module({
   imports: [forwardRef(() => UserModule)],
   controllers: [AuthController],
